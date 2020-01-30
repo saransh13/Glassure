a2e41ed7e8598f5588088729c9dce11b231ecf06
Saransh <saransh1@llnl.gov>
2020-01-30 14:23:11 -0800

    adding code for pinkbeam graphics panel

---
+++ glassure/gui/widgets/control/pinkbeam.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from ...qt import QtWidgets, QtCore
+
+from ..custom import FlatButton, HorizontalLine, LabelAlignRight
+
+
+class PinkBeamWidget(QtWidgets.QWidget):
+    def __init__(self, *args):
+        super(PinkBeamWidget, self).__init__(*args)
+
+        self.create_widgets()
+        self.create_layout()
+        self.style_widgets()
+        self.create_signals()
+
+    def create_widgets(self):
+        self.load_xray_spectrum = QtWidgets.QPushButton("Load xray Spectrum")
+
+    def create_layout(self):
+
+        self.pinkbeam_layout = QtWidgets.QHBoxLayout()
+        self.activate_cb = QtWidgets.QCheckBox("activate")
+        self.pinkbeam_layout.addWidget(self.activate_cb)
+        self.pinkbeam_layout.addWidget(self.load_xray_spectrum)
+        self.setLayout(self.pinkbeam_layout)
+
+    def style_widgets(self):
+        self.load_xray_spectrum.setFlat(True)
+
+    def create_signals(self):
+        pass
